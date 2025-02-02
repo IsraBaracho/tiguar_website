50c6021df83e4d985d11a9783e68d17663294a26
israbaracho <israbaracho@gmail.com>
2025-02-02 16:59:33 -0300

    First Row

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,26 @@ module.exports = {
     title: `tiguar_website`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
+  plugins: [
+    "gatsby-plugin-sitemap",
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Comfortaa`,
+            file: `https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/Camarao.svg"
+      }
     }
-  }]
+  ]
 };
